59c37d179702066b786745892d281413d58cc281
Jordan Henderson <hendersonj219@gmail.com>
2018-08-31 17:48:11 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>This is Jordan Henderson's 'hello world' app</p>
+    <p>Looking forward to learning how this stuff works!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
